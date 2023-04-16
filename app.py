2f0607f047339ef6fbacdd47b8a6da9489fcbd21
Aquarius-zero <130729604+Aquarius-zero@users.noreply.github.com>
2023-04-16 13:35:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 API_KEY_WEATHER = 'DFE1F06F78594F709C3184958230504'
 FB_API_URL = 'https://graph.facebook.com/v13.0/me/messages'
-PAGE_ACCESS_TOKEN = "EAAJQwAVS4moBAMRQiLZBpJ0qlPrEt5uGimi6DBHGdZAX30OCZA1U7ZAFO1ZB1hqYaZALiBlZB0ZCHzmd4eiWckcotdRjDIfoWnEX5FdXDZBKQqoZA3Rd3ZBPAO6emOASvzthsA3QJO6YU1us3WjovYPu6XaJ1nfXt5Co3hbmDoTHhslEkeoPy44HPCHivZCz0dCBuD7LAbZA3zHWjYQZDZD"
+PAGE_ACCESS_TOKEN = PASSACESSTOKEN
 bot = Bot(PAGE_ACCESS_TOKEN)
 
 list_input = ["help","hi","hello","xin chào","chào","chào bạn"]
@@ -51,108 +51,115 @@ def webhook():
     log(data)
     messenger_error=None
     processing = True
-    if data['object'] == "page" and processing:
-        processing =False
-        entries = data['entry']
-        for entry in entries:
-            sender_id = entry['messaging'][0]['sender']['id'] #lấy id của người dùng
-            try:
-                messenger_text = str(entry['messaging'][0]['message']['text']) #lấy nội dung tin nhắn từ người dùng
-            except:bot.send_text_message(sender_id,f"Xin lỗi tôi không hiểu\nVui lòng nhắn help để xem hướng dẫn")
-            recipient_id = entry['messaging'][0]['recipient']['id'] # id của chatbot
-            timesstamp = entry['messaging'][0]['timestamp']
-            if messenger_text.lower() in list_input:  # hướng dẫn người dùng
-                bot.send_text_message(sender_id,f"Chào bạn!\n" \
-           f"Tôi là chatbot giúp xem thời tiết 63 tỉnh thành tại Việt Nam:\n" )
-                bot.send_text_message(sender_id,messages_users.help())
-                break
-            try:
-                name_city = messages_users.location_input(messenger_text) # xử lý để lấy tên của tỉnh
-                lists_data_input = messages_users.data_input(messenger_text) # tách các dữ liệu ngày , giờ
-                if isinstance(lists_data_input,str):
-                    messenger_error = lists_data_input # thông báo nếu lỗi về ngày/giờ
-                    break
-
-                string_city_input = lists_data_input[0]
-                date_now = datetime.date.today()
-                dateinput = lists_data_input[1]
-                time_input = lists_data_input[2]
-                print(dateinput)
-                days = messages_users.getdays(lists_data_input,dateinput) # số ngày dự báo
-                if isinstance(days,str):
-                    messenger_error = days # thông báo lỗi từ người dùng
-                    break
-                date_input = ""
-
-                if isinstance(dateinput,list):
-                    date_input = dateinput[0]
-                else: date_input = dateinput
-
-                date_bieu_do = date_input
-                if "biểu đồ" in messenger_text:
-                    list_max = []
-                    list_min = []
-                    list_day= []
-                    for i in range(0, days + 1):
-                        response_weather =""
-                        print(date_bieu_do)
-                        if date_now < date_bieu_do:
-                            response_weather = geturl_history_or_forecast(API_KEY_WEATHER, name_city,date_bieu_do,"forecast.json").json()
-                        else:
-                            response_weather = geturl_history_or_forecast(API_KEY_WEATHER, name_city,date_bieu_do,"history.json").json()
-                        list_max.append(response_weather['forecast']['forecastday'][0]['day']['maxtemp_c'])
-                        list_min.append(response_weather['forecast']['forecastday'][0]['day']['mintemp_c'])
-                        list_day.append(response_weather['forecast']['forecastday'][0]['date'][-5:])
-                        date_bieu_do = date_bieu_do + datetime.timedelta(days=1)
-
-                    Send_image.send_image(PAGE_ACCESS_TOKEN,sender_id,Send_image.layattachment_id(PAGE_ACCESS_TOKEN,
-                                        CreatImage.Bieudo(list_max,list_min,list_day,name_city,sender_id,response_weather['location']['localtime_epoch'])))
-                    break
-                for i in range(0,days+1):
-                    if date_now == date_input:
-                        # thời tiết hiện tại
-                        if time_input == "None":
-                            response_weather= geturl_current(API_KEY_WEATHER,name_city).json()
-                            list_weather_current = weather.weather_current(response_weather,name_city)
-                            text = f"Thời tiết {name_city} vào {list_weather_current[1]}:\n" + \
-                                   messages_users.output_data_current(list_weather_current,name_city)
-                            print(text)
-                            Send_image.send_image(PAGE_ACCESS_TOKEN,sender_id,Send_image.layattachment_id(
-                                PAGE_ACCESS_TOKEN,CreatImage.image_current(list_weather_current,name_city,sender_id,response_weather['location']['localtime_epoch'])))
-                            bot.send_text_message(sender_id, text)
-
+    try:
+        if data['object'] == "page" and processing:
+            processing =False
+            entries = data['entry']
+            for entry in entries:
+                sender_id = entry['messaging'][0]['sender']['id'] #lấy id của người dùng
+                try:
+                    messenger_text = str(entry['messaging'][0]['message']['text']) #lấy nội dung tin nhắn từ người dùng
+                except:bot.send_text_message(sender_id,f"Xin lỗi tôi không hiểu\nVui lòng nhắn help để xem hướng dẫn")
+                recipient_id = entry['messaging'][0]['recipient']['id'] # id của chatbot
+                #timesstamp = entry['messaging'][0]['timestamp']
+                if messenger_text.lower() in list_input:  # hướng dẫn người dùng
+                    if messenger_text.lower() != "help":
+                        bot.send_text_message(sender_id,f"Chào bạn!\n" \
+                        f"Tôi là chatbot giúp xem thời tiết 63 tỉnh thành tại Việt Nam\n"\
+                        f"Vui lòng chat help để xem hướng dẫn.")
                     else:
-                        if date_input < date_now:
-                            response_weather = geturl_history_or_forecast(API_KEY_WEATHER, name_city, date_input,"history.json").json()
-                        else: response_weather = geturl_history_or_forecast(API_KEY_WEATHER, name_city, date_input,"forecast.json").json()
+                        bot.send_text_message(sender_id,messages_users.help())
+                    break
+                try:
+                    name_city = messages_users.location_input(messenger_text) # xử lý để lấy tên của tỉnh
+                    print(name_city)
+                    lists_data_input = messages_users.data_input(messenger_text) # tách các dữ liệu ngày , giờ
+                    if isinstance(lists_data_input,str):
+                        messenger_error = lists_data_input # thông báo nếu lỗi về ngày/giờ
+                        break
 
-                        if time_input == 'None':
-                            # nếu không có giờ cụ thể
-                            list_weather_day = weather.weather_day(response_weather, name_city)
-                            list_weather_day2 = weather.getsun_mood(response_weather)
-                            text =f"Thời tiết {name_city} trong ngày {date_input}:\n"+  \
-                                  messages_users.output_data_day(list_weather_day, name_city) # tạo nội dung
+                    string_city_input = lists_data_input[0]
+                    date_now = datetime.date.today()
+                    dateinput = lists_data_input[1]
+                    time_input = lists_data_input[2]
+                    print(dateinput)
+                    days = messages_users.getdays(lists_data_input,dateinput) # số ngày dự báo
+                    if isinstance(days,str):
+                        messenger_error = days # thông báo lỗi từ người dùng
+                        break
+                    date_input = ""
 
+                    if isinstance(dateinput,list):
+                        date_input = dateinput[0]
+                    else: date_input = dateinput
 
-                            Send_image.send_image(PAGE_ACCESS_TOKEN,sender_id,Send_image.layattachment_id(
-                            PAGE_ACCESS_TOKEN,CreatImage.image_day(list_weather_day,list_weather_day2,name_city,sender_id,response_weather['location']['localtime_epoch'])))
-                            bot.send_text_message(sender_id,text) #gửi nội dung
+                    date_bieu_do = date_input
+                    if "biểu đồ" in messenger_text:
+                        list_max = []
+                        list_min = []
+                        list_day= []
+                        for i in range(0, days + 1):
+                            response_weather =""
+                            print(date_bieu_do)
+                            if date_now < date_bieu_do:
+                                response_weather = geturl_history_or_forecast(API_KEY_WEATHER, name_city,date_bieu_do,"forecast.json").json()
+                            else:
+                                response_weather = geturl_history_or_forecast(API_KEY_WEATHER, name_city,date_bieu_do,"history.json").json()
+                            list_max.append(response_weather['forecast']['forecastday'][0]['day']['maxtemp_c'])
+                            list_min.append(response_weather['forecast']['forecastday'][0]['day']['mintemp_c'])
+                            list_day.append(response_weather['forecast']['forecastday'][0]['date'][-5:])
+                            date_bieu_do = date_bieu_do + datetime.timedelta(days=1)
+
+                        Send_image.send_image(PAGE_ACCESS_TOKEN,sender_id,Send_image.layattachment_id(PAGE_ACCESS_TOKEN,
+                                            CreatImage.Bieudo(list_max,list_min,list_day,name_city,sender_id,response_weather['location']['localtime_epoch'])))
+                        break
+                    for i in range(0,days+1):
+                        if date_now == date_input:
+                            # thời tiết hiện tại
+                            if time_input == "None":
+                                response_weather= geturl_current(API_KEY_WEATHER,name_city).json()
+                                list_weather_current = weather.weather_current(response_weather,name_city)
+                                text = f"Thời tiết {name_city} vào {list_weather_current[1]}:\n" + \
+                                       messages_users.output_data_current(list_weather_current,name_city)
+                                print(text)
+                                Send_image.send_image(PAGE_ACCESS_TOKEN,sender_id,Send_image.layattachment_id(
+                                    PAGE_ACCESS_TOKEN,CreatImage.image_current(list_weather_current,name_city,sender_id,response_weather['location']['localtime_epoch'])))
+                                bot.send_text_message(sender_id, text)
+
                         else:
-                            #nếu có giờ cụ thể
-                            list_weather_hour = weather.weather_hour(response_weather,str(date_input),time_input,name_city)
-                            text = f"Thời tiết {name_city} vào {date_input} {time_input}:\n" + messages_users.output_data_hour(
-                                                                                  list_weather_hour,name_city)
-                            Send_image.send_image(PAGE_ACCESS_TOKEN,sender_id,Send_image.layattachment_id(
-                                PAGE_ACCESS_TOKEN,CreatImage.image_hour(list_weather_hour,name_city,sender_id,response_weather['location']['localtime_epoch'])))
-                            bot.send_text_message(sender_id,text)
-                    date_input = date_input + datetime.timedelta(days=1) # ngày tiếp theo nếu có
+                            if date_input < date_now:
+                                response_weather = geturl_history_or_forecast(API_KEY_WEATHER, name_city, date_input,"history.json").json()
+                            else: response_weather = geturl_history_or_forecast(API_KEY_WEATHER, name_city, date_input,"forecast.json").json()
 
-            except Exception as e:
-                print("Đã có lỗi xảy ra:", e)
-                if messenger_error == None:
-                    bot.send_text_message(sender_id,f"Xin lỗi tôi không hiểu\nVui lòng nhắn help để xem hướng dẫn")
-        if messenger_error != None:
-            bot.send_text_message(sender_id, messenger_error)
+                            if time_input == 'None':
+                                # nếu không có giờ cụ thể
+                                list_weather_day = weather.weather_day(response_weather, name_city)
+                                list_weather_day2 = weather.getsun_mood(response_weather)
+                                text =f"Thời tiết {name_city} trong ngày {date_input}:\n"+  \
+                                      messages_users.output_data_day(list_weather_day, name_city) # tạo nội dung
+
+
+                                Send_image.send_image(PAGE_ACCESS_TOKEN,sender_id,Send_image.layattachment_id(
+                                PAGE_ACCESS_TOKEN,CreatImage.image_day(list_weather_day,list_weather_day2,name_city,sender_id,response_weather['location']['localtime_epoch'])))
+                                bot.send_text_message(sender_id,text) #gửi nội dung
+                            else:
+                                #nếu có giờ cụ thể
+                                list_weather_hour = weather.weather_hour(response_weather,str(date_input),time_input,name_city)
+                                text = f"Thời tiết {name_city} vào {date_input} {time_input}:\n" + messages_users.output_data_hour(
+                                                                                      list_weather_hour,name_city)
+                                Send_image.send_image(PAGE_ACCESS_TOKEN,sender_id,Send_image.layattachment_id(
+                                    PAGE_ACCESS_TOKEN,CreatImage.image_hour(list_weather_hour,name_city,sender_id,response_weather['location']['localtime_epoch'])))
+                                bot.send_text_message(sender_id,text)
+                        date_input = date_input + datetime.timedelta(days=1) # ngày tiếp theo nếu có
+
+                except Exception as e:
+                    print("Đã có lỗi xảy ra:", e)
+                    if messenger_error == None:
+                        bot.send_text_message(sender_id,f"Xin lỗi tôi không hiểu\nVui lòng nhắn help để xem hướng dẫn")
+            if messenger_error != None:
+                bot.send_text_message(sender_id, messenger_error)
+    except:
+        print("lỗi tin nhắn")
 
     return "OK"
 def log(message):
